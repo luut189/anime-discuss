25ea566d49bc2fd64fabae4ee6caca36030d480c
luut189 <minhtuongluu74@gmail.com>
2025-01-26 04:44:18 -0500

    feat: added a simple frontend to test the api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,83 @@
 import { useState } from 'react';
+import { JikanAnimeData, JikanPaginationData } from '@/common/interfaces';
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+interface JikanData {
+    data: JikanAnimeData[];
+    pagination: JikanPaginationData;
+}
 
 export default function App() {
-    const [count, setCount] = useState(0);
+    const [data, setData] = useState<JikanData>();
+    const [page, setPage] = useState(0);
+    async function handleClick() {
+        try {
+            const response = await fetch('http://localhost:5000/anime/today');
+            if (!response.ok) {
+                throw new Error();
+            }
+            const newData = await response.json();
+            setData(newData);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <div className='flex min-h-screen flex-col items-center justify-center gap-2 bg-blue-50'>
-            <h1 className='text-3xl font-bold'>Vite + React + TS + TailwindCSS</h1>
-            <button
-                className='rounded-md border bg-slate-50 p-2 shadow-sm hover:bg-slate-100'
-                onClick={() => setCount((count) => count + 1)}>
-                Count is {count}
-            </button>
-            <div className='flex flex-col items-center justify-center gap-2 rounded-lg border bg-slate-50 p-5 shadow-sm'>
-                <div className='flex flex-col items-center justify-center gap-2'>
-                    <p>
-                        Edit{' '}
-                        <code className='rounded-md bg-slate-200 p-1 shadow-sm'>src/App.tsx</code>{' '}
-                        and save to test HMR
-                    </p>
-                    <p>
-                        This template app already includes support for{' '}
-                        <span className='font-bold'>shadcn/ui</span> so you can just add new
-                        component by
-                    </p>
-                    <p className='rounded-md bg-slate-200 p-2 shadow-sm'>
-                        <code>npx shadcn@latest add {'<component>'}</code>
-                    </p>
-                </div>
+        <div className='flex flex-col items-center justify-center gap-5 p-5'>
+            <div className='flex items-center justify-center gap-5'>
+                <Button
+                    onClick={() => {
+                        setPage(page == 0 ? 0 : page - 1);
+                    }}
+                    size={'icon'}>
+                    <ChevronLeft />
+                </Button>
+                <Button variant={'outline'} disabled className='disabled:opacity-100'>
+                    {page + 1} / {data?.pagination.last_visible_page}
+                </Button>
+                <Button
+                    onClick={() => {
+                        setPage(page + 1 == data?.pagination.last_visible_page ? 0 : page + 1);
+                    }}
+                    size={'icon'}>
+                    <ChevronRight />
+                </Button>
+            </div>
+            <Button variant={'outline'} onClick={handleClick}>
+                Get Today Anime
+            </Button>
+            <div className='flex flex-row flex-wrap items-center justify-center gap-2'>
+                {data?.data
+                    .slice(
+                        page * data.pagination.items.per_page,
+                        (page + 1) * data.pagination.items.per_page,
+                    )
+                    .map((anime, index) => (
+                        <Card
+                            key={index}
+                            className='flex flex-col justify-between p-4 w-full min-h-max max-w-xs hover:bg-gray-100 transition-all'>
+                            <CardHeader>
+                                <CardTitle className='break-words text-lg font-semibold text-center'>
+                                    {anime.title || 'Untitled'}
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent className='flex justify-center items-center'>
+                                <img
+                                    src={anime.images.jpg.large_image_url}
+                                    alt={anime.title_english || 'Anime Image'}
+                                    className='w-full object-cover rounded-md'
+                                />
+                            </CardContent>
+                        </Card>
+                    ))}
             </div>
         </div>
     );
