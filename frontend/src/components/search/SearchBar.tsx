09c98eb970186f0a05797a2621884937ae5a3997
luut189 <73666044+luut189@users.noreply.github.com>
2025-03-03 21:05:42 -0500

    feat(search-bar): improved search bar and now user can pin search bar results as well

--- frontend/src/components/search/SearchBar.tsx
+++ frontend/src/components/search/SearchBar.tsx
@@ -5,17 +5,15 @@ import { JikanAnimeData } from '@/common/interfaces';
 import SearchResult from '@/components/search/SearchResult';
 
 import type React from 'react';
-import { useEffect, useRef, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useQuery } from '@tanstack/react-query';
 
 export default function SearchBar() {
     const navigate = useNavigate();
-    const location = useLocation();
     const [searchQuery, setSearchQuery] = useState('');
     const [debouncedQuery, setDebouncedQuery] = useState(searchQuery);
     const [showResult, setShowResult] = useState(true);
-    const blurTimeoutRef = useRef<NodeJS.Timeout>();
 
     const { isError, isPending, data } = useQuery({
         queryKey: ['search-result', debouncedQuery, 1],
@@ -33,12 +31,6 @@ export default function SearchBar() {
         return () => clearTimeout(timer);
     }, [searchQuery]);
 
-    // clear search query when changing location
-    useEffect(() => {
-        setSearchQuery('');
-        setDebouncedQuery('');
-    }, [location]);
-
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         e.preventDefault();
         setSearchQuery(e.target.value);
@@ -50,38 +42,33 @@ export default function SearchBar() {
     }
 
     function handleBlur() {
-        if (blurTimeoutRef.current) {
-            clearTimeout(blurTimeoutRef.current);
-        }
-
-        blurTimeoutRef.current = setTimeout(() => {
-            setShowResult(false);
-        }, 300);
+        setShowResult(false);
     }
 
     function handleFocus() {
-        if (blurTimeoutRef.current) {
-            clearTimeout(blurTimeoutRef.current);
-        }
         setShowResult(true);
     }
 
     return (
         <div className='flex w-1/2 flex-col'>
-            <form className='flex items-center justify-center' onSubmit={handleSubmit}>
+            <form
+                className='flex items-center justify-center'
+                onSubmit={handleSubmit}
+                onBlur={handleBlur}
+                onFocus={handleFocus}>
                 <Input
                     type='text'
                     placeholder='Search...'
                     value={searchQuery}
                     onChange={handleChange}
-                    onBlur={handleBlur}
-                    onFocus={handleFocus}
                     className='flex-grow'
                 />
             </form>
             <div className='relative'>
                 {showResult && debouncedQuery.length > 0 && (
-                    <div className='absolute z-10 mt-2 w-full'>
+                    <div
+                        className='absolute z-10 mt-2 w-full'
+                        onMouseDown={(e) => e.preventDefault()}>
                         <SearchResult
                             isError={isError}
                             isPending={isPending}
--- frontend/src/components/search/SearchResult.tsx
+++ frontend/src/components/search/SearchResult.tsx
@@ -2,6 +2,7 @@ import { JikanAnimeData } from '@/common/interfaces';
 import { Card } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { PinAnimeButton } from '@/components/anime/PinAnimeButton';
 
 import { useNavigate } from 'react-router';
 
@@ -46,22 +47,33 @@ export default function SearchResult({ isError, isPending, data }: SearchResultP
                 {data.map((anime) => (
                     <li
                         key={anime.mal_id}
-                        className='flex w-full cursor-pointer flex-col rounded border-b p-2 transition-all last:border-b-0 last:pb-0 hover:border'
-                        onClick={() => {
-                            navigate(`/anime/${anime.mal_id}`);
-                        }}>
+                        className='flex w-full cursor-pointer flex-col rounded border-b p-2 transition-all last:border-b-0 last:pb-0 hover:border'>
                         <TooltipProvider>
                             <Tooltip>
-                                <TooltipTrigger>
-                                    <div className='line-clamp-2 break-words text-start font-bold'>
-                                        {anime.title || 'Untitled'}
+                                <TooltipTrigger className='flex items-center justify-between'>
+                                    <div
+                                        onClick={() => {
+                                            navigate(`/anime/${anime.mal_id}`);
+                                        }}
+                                        className='w-2/3'>
+                                        <div className='line-clamp-2 break-words text-start font-bold'>
+                                            {anime.title || 'Untitled'}
+                                        </div>
+                                        <div className='line-clamp-2 break-words text-start text-sm'>
+                                            {anime.title_japanese || 'Unknown Japanese Title'}
+                                        </div>
                                     </div>
-                                    <div className='line-clamp-2 break-words text-start text-sm'>
-                                        {anime.title_japanese || 'Unknown Japanese Title'}
-                                    </div>
+                                    <PinAnimeButton {...anime} />
                                 </TooltipTrigger>
-                                <TooltipContent>
-                                    <p>{anime.title}</p>
+                                <TooltipContent className='flex flex-col items-center justify-center gap-2'>
+                                    <img
+                                        src={anime.images.webp.image_url}
+                                        alt={anime.title}
+                                        className='h-48 w-32'
+                                    />
+                                    <p className='w-48 text-wrap text-center font-semibold'>
+                                        {anime.title}
+                                    </p>
                                 </TooltipContent>
                             </Tooltip>
                         </TooltipProvider>
