b7d231626856b44fe434bc843f2e4a3a20274f7f
luut189 <minhtuongluu74@gmail.com>
2025-04-25 14:52:03 +0700

    fix: removed password hint in login page

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -14,18 +14,7 @@ const formSchema = z.object({
     username: z.string().min(2, {
         message: 'Username must be at least 2 characters.',
     }),
-    password: z
-        .string()
-        .min(8, { message: 'Password must be at least 8 characters.' })
-        .refine((password) => /[0-9]/.test(password), {
-            message: 'Password must contain at least a number.',
-        })
-        .refine((password) => /[a-z]/.test(password), {
-            message: 'Password must contain at least an alphabet character.',
-        })
-        .refine((password) => /[A-Z]/.test(password), {
-            message: 'Password must contain at least an uppercase character.',
-        }),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters.' }),
 });
 
 interface IFormItem {
