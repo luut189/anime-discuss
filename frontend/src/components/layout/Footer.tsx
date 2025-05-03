674d1b796cb85e8f522fc931a6e67a96b6f1aabe
luut189 <minhtuongluu74@gmail.com>
2025-05-03 14:17:04 +0700

    feat: enhanced footer with attribution link and styling

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,7 +1,15 @@
 export default function Footer() {
     return (
         <footer className='flex items-center justify-center p-4 text-xs text-muted-foreground'>
-            &copy; {new Date().getFullYear()} Tuong Luu. All Rights Reserved.
+            &copy; {new Date().getFullYear()} and built with ❤️ by{' '}
+            <a
+                href='http://github.com/luut189'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='ml-1 underline underline-offset-2 hover:text-primary'>
+                Tuong Luu
+            </a>
+            . All Rights Reserved.
         </footer>
     );
 }
